64e85017f71c3d03ea14575a00290f582d472bb1
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-01-19 09:58:41 +0530

    power

--- 01_control.js
+++ 01_control.js
@@ -1,8 +1,9 @@
 const isUserloggedIn = true
+const temperature = 41
  
-if (isUserloggedIn) {
+// if (isUserloggedIn) {
     
-}
+// }
 
 //operator //
 // < less than   5 < 6
@@ -10,11 +11,49 @@ if (isUserloggedIn) {
 // 2<=2 is 2 is smaller or equal or not
 // 2>=2  is 2 is greter or equal or not
 // == checking two value or equal or not
-//
-//
-//
-//
-//
-//
-//
+// <, >, <=, >=, ==, !=, ===, !==
+
+//if ( temperature === 34) {
+   console.log("less than 50")
+
+//} else {
+ console.log( "it is in minus temperature~")
+//}
+//console.log( "Execute")
+
+const score = 200 
+
+if ( score > 100) {
+   const power = "fly"
+   console.log(`User power: ${power}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
