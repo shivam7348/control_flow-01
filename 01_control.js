440e1e7b4f19f5a8ab9254bca0a5665c3aaa6dcf
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-01-19 09:12:35 +0530

    first_controlFlow

---
+++ 01_control.js
@@ -0,0 +1,20 @@
+const isUserloggedIn = true
+ 
+if (isUserloggedIn) {
+    
+}
+
+//operator //
+// < less than   5 < 6
+// > greter than 5 > 9
+// 2<=2 is 2 is smaller or equal or not
+// 2>=2  is 2 is greter or equal or not
+// == checking two value or equal or not
+//
+//
+//
+//
+//
+//
+//
+
